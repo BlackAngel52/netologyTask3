946ec1798edbec74f2f12bd451901858211df3ce
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-04-22 20:23:23 +0300

    Add files via upload

---
+++ netologyTask3_1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+int main()
+{
+    bool first_bool = { true };
+    bool second_bool = { true };
+    // Таблица истинности для оператора ||
+
+    std::cout << std::boolalpha << "Operator: ||" << std::endl;
+    std::cout << first_bool << " " << second_bool << " " << (first_bool || second_bool) << std::endl;
+
+    first_bool = { false };
+
+    std::cout << first_bool << " " << second_bool << " " << (first_bool || second_bool) << std::endl;
+
+    first_bool = { true };
+    second_bool = { false };
+
+    std::cout << first_bool << " " << second_bool << " " << (first_bool || second_bool) << std::endl;
+
+    first_bool = { false };
+
+    std::cout << first_bool << " " << second_bool << " " << (first_bool || second_bool) << std::endl;
+
+    std::cout << std::endl;
+
+
+    //Таблица истинности для оператора &&
+
+    first_bool = { true };
+    second_bool = { true };
+
+    std::cout << "Operator: &&" << std::endl;
+    std::cout << first_bool << " " << second_bool << " " << (first_bool && second_bool) << std::endl;
+
+    first_bool = { false };
+
+    std::cout << first_bool << " " << second_bool << " " << (first_bool && second_bool) << std::endl;
+
+    first_bool = { true };
+    second_bool = { false };
+
+    std::cout << first_bool << " " << second_bool << " " << (first_bool && second_bool) << std::endl;
+
+    first_bool = { false };
+
+    std::cout << first_bool << " " << second_bool << " " << (first_bool && second_bool) << std::endl;
+
+}
+
---
+++ netologyTask3_2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+int main()
+{
+    int firstNum, secondNum, thirdNum{};
+
+    std::cout << "Enter first number: ";
+    std::cin >> firstNum;
+    std::cout << std::endl;
+    std::cout << "Enter second number: ";
+    std::cin >> secondNum;
+    std::cout << std::endl; 
+    std::cout << "Enter third number: ";
+    std::cin >> thirdNum;
+    std::cout << std::endl;
+
+
+    //firstNum >= secondNum ? 
+
+    if (firstNum >= secondNum) 
+    {
+        if (firstNum >= thirdNum) 
+        {
+            if (secondNum >= thirdNum) 
+            {
+                std::cout << firstNum << " " << secondNum << " " << thirdNum << std::endl;
+            }
+            else 
+            {
+                std::cout << firstNum << " " << thirdNum << " " << secondNum << std::endl;
+            }
+        }
+        else 
+        {
+            std::cout << thirdNum << " " << firstNum << " " << secondNum << std::endl;
+        }
+    }
+    else
+    {
+        if (firstNum >= thirdNum) 
+        {
+            std::cout << secondNum << " " << firstNum << " " << thirdNum << std::endl;
+        }
+        else if (secondNum >= thirdNum) 
+        {
+            std::cout << secondNum << " " << thirdNum << " " << firstNum << std::endl;
+        }
+        else
+        {
+            std::cout << thirdNum << " " << secondNum << " " << firstNum << std::endl;
+        }
+    }
+
+
+}
+
